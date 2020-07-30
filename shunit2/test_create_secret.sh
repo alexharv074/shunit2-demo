fa32cd0c9337ee564d5f92791eebda8f4dc52ca7
Alex Harvey <alexharv074@gmail.com>
2020-07-31 02:17:14 +1000

    Stage 11 - add a fairly useless mock

--- shunit2/test_create_secret.sh
+++ shunit2/test_create_secret.sh
@@ -2,8 +2,19 @@
 
 under_test='./create_secret.sh'
 
-testEquality() {
-  assertEquals "1" "1"
+aws() {
+  cat <<EOF
+{
+  "ARN": "arn:aws:secretsmanager:us-west-2:123456789012:secret:MyTestDatabaseSecret-a1b2c3",
+  "Name": "MyTestDatabaseSecret",
+  "VersionId": "EXAMPLE1-90ab-cdef-fedc-ba987EXAMPLE"
+}
+EOF
+}
+
+testMain() {
+  (. "$under_test" ; SECRET_NAME=MyTestDatabaseSecret SECRET_DESC="My test" SECRET=abcd1234 main)
+  assertTrue "script did not return exit status 0" "$?"
 }
 
 . shunit2
