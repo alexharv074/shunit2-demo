dbcb3852150b4c6cd92f2f0a29d5119ec17c5996
Alex Harvey <alexharv074@gmail.com>
2020-07-30 23:53:32 +1000

    Stage 6 - add a setUp

--- shunit2/test_squared.sh
+++ shunit2/test_squared.sh
@@ -2,20 +2,21 @@
 
 under_test='./squared.sh'
 
-test3Squared() {
+setUp() {
   . "$under_test"
+}
+
+test3Squared() {
   local n_sq="$(main 3)"
   assertEquals "did not compute 3^2" "$n_sq" "9"
 }
 
 test4Squared() {
-  . "$under_test"
   local n_sq="$(main 4)"
   assertEquals "did not compute 4^2" "$n_sq" "16"
 }
 
 testUsage() {
-  . "$under_test"
   main | grep -qi usage
   assertTrue "did not see usage" "$?"
 }
