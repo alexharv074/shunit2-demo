cdae9f1be7ef226397b8fa7ee22ba02d5866d33f
Alex Harvey <alexharv074@gmail.com>
2020-07-31 02:37:36 +1000

    Stage 12 - add spies to properly test behaviour

--- shunit2/test_create_secret.sh
+++ shunit2/test_create_secret.sh
@@ -3,6 +3,7 @@
 under_test='./create_secret.sh'
 
 aws() {
+  echo "aws $@" > commands_log
   cat <<EOF
 {
   "ARN": "arn:aws:secretsmanager:us-west-2:123456789012:secret:MyTestDatabaseSecret-a1b2c3",
@@ -14,7 +15,17 @@ EOF
 
 testMain() {
   (. "$under_test" ; SECRET_NAME=MyTestDatabaseSecret SECRET_DESC="My test" SECRET=abcd1234 main)
-  assertTrue "script did not return exit status 0" "$?"
+
+  local expected_behaviour="aws secretsmanager create-secret \
+--name MyTestDatabaseSecret --description My test --secret-string abcd1234"
+
+  local actual_behaviour="$(<commands_log)"
+
+  assertEquals "unexpected behaviour" "$expected_behaviour" "$actual_behaviour"
+}
+
+tearDown() {
+  rm -f commands_log
 }
 
 . shunit2
