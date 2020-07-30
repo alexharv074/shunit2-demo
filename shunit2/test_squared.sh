3b4cf0634a6dd311a5dbfd60795a6531ee63cf09
Alex Harvey <alexharv074@gmail.com>
2020-07-30 23:48:07 +1000

    Stage 5 - test at function level

--- shunit2/test_squared.sh
+++ shunit2/test_squared.sh
@@ -3,17 +3,20 @@
 under_test='./squared.sh'
 
 test3Squared() {
-  local n_sq="$(bash "$under_test" 3)"
+  . "$under_test"
+  local n_sq="$(main 3)"
   assertEquals "did not compute 3^2" "$n_sq" "9"
 }
 
 test4Squared() {
-  local n_sq="$(bash "$under_test" 4)"
+  . "$under_test"
+  local n_sq="$(main 4)"
   assertEquals "did not compute 4^2" "$n_sq" "16"
 }
 
 testUsage() {
-  bash "$under_test" | grep -qi usage
+  . "$under_test"
+  main | grep -qi usage
   assertTrue "did not see usage" "$?"
 }
 
